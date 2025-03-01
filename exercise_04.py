d20e88bfa42159f7f06fade72d26a39c2d5995a5
rosele21 <>
2025-03-01 16:03:05 +0800

    Last Sync: 2025-03-01 08:03:05 (Mobile)

---
+++ exercise_04.py
@@ -0,0 +1,20 @@
+import math
+number = 16
+sqrt_result = math.sqrt(number)
+pi_value = math.pi
+angle_degrees = 30
+angle_radians = math.radians(angle_degrees)
+sin_result = math.sin(angle_radians)
+cos_result = math.cos(angle_radians)
+tan_result = math.tan(angle_radians)
+exp_result = math.exp(2)
+log_result = math.log(10)  # Natural log (base e)
+log10_result = math.log(100, 10) # Log base 10
+print("Square root of", number, "is:", sqrt_result)
+print("Value of pi is:", pi_value)
+print("Sine of 30 degrees (in radians) is:", sin_result)
+print("Cosine of 60 degrees (in radians) is:", cos_result)
+print("Tangent of 45 degrees (in radians) is:", tan_result)
+print("Exponential of 2 is:", exp_result)
+print("Logarithm (base e) of 10 is:", log_result)
+print("Logarithm (base 10) of 100 is:", log10_result)
